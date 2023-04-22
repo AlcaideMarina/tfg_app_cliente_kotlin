12f399de0a158f573428c389039837b90b8ff200
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-22 02:21:15 +0200

    feat: Create base fragment

---
+++ app/src/main/java/com/example/hueverianietoclientes/base/BaseFragment.kt
@@ -0,0 +1,51 @@
+package com.example.hueverianietoclientes.base
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    protected abstract fun configureUI()
+
+    protected abstract fun setObservers()
+
+    protected abstract fun setListeners()
+
+    protected abstract fun updateUI(state: BaseState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(
+            this.javaClass.simpleName,
+            "Calling fragment onCreate(): " + this.javaClass.simpleName
+        )
+        setObservers()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configureUI()
+        setListeners()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(this.javaClass.simpleName, "Calling fragment onStart(): " + this.javaClass.simpleName)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(
+            this.javaClass.simpleName,
+            "Calling fragment onResume(): " + this.javaClass.simpleName
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(this.javaClass.simpleName, "Calling fragment onStop(): " + this.javaClass.simpleName)
+    }
+
+}
