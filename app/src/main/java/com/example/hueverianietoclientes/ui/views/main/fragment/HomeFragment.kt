32d9bcb676e7c1665a5b78b8366ace3e0fa12f6f
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-22 20:29:09 +0200

    feat: Add recyclerview in menu screen

--- app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/HomeFragment.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hueverianietoclientes.R
 import com.example.hueverianietoclientes.base.BaseFragment
 import com.example.hueverianietoclientes.base.BaseState
 import com.example.hueverianietoclientes.databinding.FragmentHomeBinding
+import com.example.hueverianietoclientes.domain.model.GridItemModel
+import com.example.hueverianietoclientes.ui.components.hngridview.CustomGridLayoutManager
+import com.example.hueverianietoclientes.ui.components.hngridview.HNGridViewAdapter
 import com.example.hueverianietoclientes.ui.views.main.MainActivity
+
 
 class HomeFragment : BaseFragment() {
 
@@ -25,7 +35,7 @@ class HomeFragment : BaseFragment() {
     }
 
     override fun configureUI() {
-        //TODO("Not yet implemented")
+        setMenuRecyclerView()
     }
 
     override fun setObservers() {
@@ -38,6 +48,36 @@ class HomeFragment : BaseFragment() {
 
     override fun updateUI(state: BaseState) {
         // It is not necessary, this is a static view
+    }
+
+    private fun showToasst(msg : String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setMenuRecyclerView() {
+        val menuOptions: List<GridItemModel> = listOf(
+            GridItemModel("Mi perfil", AppCompatResources.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)!!) { showToasst("Pulsado 'Mi perfil'") },
+            GridItemModel("Facturación", AppCompatResources.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)!!) { showToasst("Pulsado 'Facturación'") },
+            GridItemModel("Mis pedidos", AppCompatResources.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)!!) { showToasst("Pulsado 'Mis pedidos'") },
+            GridItemModel("Nuevo pedido", AppCompatResources.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)!!) { showToasst("Pulsado 'Nuevo pedido'") },
+            GridItemModel("Ajustes", AppCompatResources.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)!!) { showToasst("Pulsado 'Ajustes'") }
+        )
+        val manager = CustomGridLayoutManager(this.context, 2)
+        manager.setScrollEnabled(false)
+        manager.spanSizeLookup = object : SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (menuOptions.size % 2 != 0) {
+                    if (position == menuOptions.size - 1) 2 else 1
+                } else {
+                    1
+                }
+            }
+        }
+
+        with(this.binding.lstItems) {
+            layoutManager = manager
+            adapter = HNGridViewAdapter(menuOptions)
+        }
     }
 
 }
