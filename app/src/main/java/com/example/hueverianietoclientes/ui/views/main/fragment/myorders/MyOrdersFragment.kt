3132e765c96a09211a03bac7a0586332cb322cc7
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-23 03:51:56 +0200

    feat: Create base of myOrdersFragment

---
+++ app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/myorders/MyOrdersFragment.kt
@@ -0,0 +1,47 @@
+package com.example.hueverianietoclientes.ui.views.main.fragment.myorders
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hueverianietoclientes.base.BaseFragment
+import com.example.hueverianietoclientes.base.BaseState
+import com.example.hueverianietoclientes.data.network.ClientData
+import com.example.hueverianietoclientes.databinding.FragmentMyOrdersBinding
+import com.example.hueverianietoclientes.ui.views.main.MainActivity
+
+class MyOrdersFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentMyOrdersBinding
+    private lateinit var clientData: ClientData
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as MainActivity).configNav(true)
+        clientData = (activity as MainActivity).clientData
+        this.binding = FragmentMyOrdersBinding.inflate(
+            inflater, container, false
+        )
+        return this.binding.root
+    }
+
+    override fun configureUI() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun setObservers() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun setListeners() {
+        // It is not necessary
+    }
+
+    override fun updateUI(state: BaseState) {
+       // It is not necessary
+    }
+
+}
