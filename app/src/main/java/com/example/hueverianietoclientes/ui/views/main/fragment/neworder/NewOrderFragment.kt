f7fd06464a375fb968638d65ec43c868e13edcbb
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-23 16:18:33 +0200

    fix: Add binding and delete todos in new order frg

--- app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/neworder/NewOrderFragment.kt
+++ app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/neworder/NewOrderFragment.kt
@@ -1,27 +1,51 @@
 package com.example.hueverianietoclientes.ui.views.main.fragment.neworder
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.hueverianietoclientes.base.BaseFragment
 import com.example.hueverianietoclientes.base.BaseState
+import com.example.hueverianietoclientes.data.network.ClientData
+import com.example.hueverianietoclientes.databinding.FragmentNewOrderBinding
+import com.example.hueverianietoclientes.ui.views.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NewOrderFragment : BaseFragment() {
 
+    private lateinit var binding: FragmentNewOrderBinding
+    private lateinit var clientData: ClientData
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as MainActivity).configNav(true)
+        this.binding = FragmentNewOrderBinding.inflate(
+            inflater, container, false
+        )
+        val args : NewOrderFragmentArgs by navArgs()
+        this.clientData = args.clientData
+
+        return this.binding.root
+    }
 
     override fun configureUI() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
     override fun setObservers() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
     override fun setListeners() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
     override fun updateUI(state: BaseState) {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 }
