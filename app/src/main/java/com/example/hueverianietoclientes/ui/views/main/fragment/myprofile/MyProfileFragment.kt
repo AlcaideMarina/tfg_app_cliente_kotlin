e5beacde6909a33b975c8cae2e50d32041c3e9b5
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-23 03:07:14 +0200

    feat: Set text fields

--- app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/myprofile/MyProfileFragment.kt
+++ app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/myprofile/MyProfileFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.hueverianietoclientes.base.BaseFragment
 import com.example.hueverianietoclientes.base.BaseState
+import com.example.hueverianietoclientes.data.network.ClientData
 import com.example.hueverianietoclientes.databinding.FragmentMyProfileBinding
 import com.example.hueverianietoclientes.ui.views.login.LoginActivity
 import com.example.hueverianietoclientes.ui.views.main.MainActivity
@@ -19,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MyProfileFragment : BaseFragment() {
 
     private lateinit var binding: FragmentMyProfileBinding
+    private lateinit var clientData: ClientData
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,12 +28,14 @@ class MyProfileFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         (activity as MainActivity).configNav("Mi perfil", true)
+        clientData = (activity as MainActivity).clientData
         this.binding = FragmentMyProfileBinding.inflate(inflater, container, false)
         return this.binding.root
     }
 
     override fun configureUI() {
-
+        setDisabledTextFields()
+        setTextFields()
     }
 
     override fun setObservers() {
@@ -44,6 +48,40 @@ class MyProfileFragment : BaseFragment() {
 
     override fun updateUI(state: BaseState) {
         // It is not necessary, this is a static screen
+    }
+
+    private fun setDisabledTextFields() {
+        with(this.binding) {
+            companyTextInputLayout.isEnabled = false
+            directionTextInputLayout.isEnabled = false
+            cityTextInputLayout.isEnabled = false
+            provinceTextInputLayout.isEnabled = false
+            postalCodeTextInputLayout.isEnabled = false
+            cifTextInputLayout.isEnabled = false
+            phoneTextInputLayoutPhone1.isEnabled = false
+            phoneTextInputLayoutName1.isEnabled = false
+            phoneTextInputLayoutPhone2.isEnabled = false
+            phoneTextInputLayoutName2.isEnabled = false
+            userTextInputLayout.isEnabled = false
+            emailTextInputLayout.isEnabled = false
+        }
+    }
+
+    private fun setTextFields() {
+        with(this.binding) {
+            companyTextInputLayout.setInputText(clientData.company)
+            directionTextInputLayout.setInputText(clientData.direction)
+            cityTextInputLayout.setInputText(clientData.city)
+            provinceTextInputLayout.setInputText(clientData.province)
+            postalCodeTextInputLayout.setInputText(clientData.postalCode.toString())
+            cifTextInputLayout.setInputText(clientData.cif)
+            phoneTextInputLayoutPhone1.setInputText("clientData.phone[0].keys")
+            phoneTextInputLayoutName1.setInputText("clientData.phone[0].values")
+            phoneTextInputLayoutPhone2.setInputText("clientData.phone[0].keys")
+            phoneTextInputLayoutName2.setInputText("clientData.phone[0].values")
+            userTextInputLayout.setInputText(clientData.user)
+            emailTextInputLayout.setInputText(clientData.email)
+        }
     }
 
     companion object {
