da29888581a502b917f99d6937a4f95289892f8a
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-23 15:40:47 +0200

    feat: Create new order fragment

---
+++ app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/neworder/NewOrderFragment.kt
@@ -0,0 +1,27 @@
+package com.example.hueverianietoclientes.ui.views.main.fragment.neworder
+
+import com.example.hueverianietoclientes.base.BaseFragment
+import com.example.hueverianietoclientes.base.BaseState
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NewOrderFragment : BaseFragment() {
+
+
+
+    override fun configureUI() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setObservers() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUI(state: BaseState) {
+        TODO("Not yet implemented")
+    }
+}
