f27ea4f7aebf13255aded14053fca801086e5d8d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-26 01:15:04 +0200

    feat: Add getordersummary function

--- app/src/main/java/com/example/hueverianietoclientes/utils/OrderUtils.kt
+++ app/src/main/java/com/example/hueverianietoclientes/utils/OrderUtils.kt
@@ -193,4 +193,26 @@ object OrderUtils {
         return dbOrderFieldData
     }
 
+    fun getOrderSummary(dbOrderFieldData: DBOrderFieldData) : String {
+
+        val list : MutableList<String> = mutableListOf()
+
+        if (dbOrderFieldData.xlBoxQuantity != 0) list.add(dbOrderFieldData.xlBoxQuantity.toString() + " cajas XL")
+        if (dbOrderFieldData.xlDozenQuantity != 0) list.add(dbOrderFieldData.xlDozenQuantity.toString() + " docenas XL")
+        if (dbOrderFieldData.lBoxQuantity != 0) list.add(dbOrderFieldData.lBoxQuantity.toString() + " cajas L")
+        if (dbOrderFieldData.lDozenQuantity != 0) list.add(dbOrderFieldData.lDozenQuantity.toString() + " docenas L")
+        if (dbOrderFieldData.mBoxQuantity != 0) list.add(dbOrderFieldData.mBoxQuantity.toString() + " cajas M")
+        if (dbOrderFieldData.mDozenQuantity != 0) list.add(dbOrderFieldData.mDozenQuantity.toString() + " docenas M")
+        if (dbOrderFieldData.sBoxQuantity != 0) list.add(dbOrderFieldData.sBoxQuantity.toString() + " cajas S")
+        if (dbOrderFieldData.sDozenQuantity != 0) list.add(dbOrderFieldData.sDozenQuantity.toString() + " docenas S")
+
+        var summary = ""
+        for (item in list) {
+            summary += item
+            if (item != list[list.size - 1]) summary += " - "
+        }
+
+        return summary
+    }
+
 }
