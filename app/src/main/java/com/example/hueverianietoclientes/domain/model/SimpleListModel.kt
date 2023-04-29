8d5ece069147cc0e39dbfbbb7773240b8f92e5e9
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-29 23:41:18 +0200

    feat: Create simple list component

---
+++ app/src/main/java/com/example/hueverianietoclientes/domain/model/SimpleListModel.kt
@@ -0,0 +1,8 @@
+package com.example.hueverianietoclientes.domain.model
+
+import android.view.View.OnClickListener
+
+class SimpleListModel (
+    val title: String,
+    val onClickListener: OnClickListener
+)
