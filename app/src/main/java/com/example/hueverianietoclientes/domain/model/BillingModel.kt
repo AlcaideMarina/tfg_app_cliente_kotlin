38a4624b6a77521f2c876cc0d6f2e4ec38c66902
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-30 20:04:23 +0200

    feat: Set billing model as parcelable

--- app/src/main/java/com/example/hueverianietoclientes/domain/model/BillingModel.kt
+++ app/src/main/java/com/example/hueverianietoclientes/domain/model/BillingModel.kt
@@ -1,7 +1,9 @@
 package com.example.hueverianietoclientes.domain.model
 
-import com.google.firebase.Timestamp
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class BillingModel (
     var paymentByCash: Number = 0,
     var paymentByReceipt: Number = 0,
@@ -9,5 +11,5 @@ data class BillingModel (
     var paid: Number = 0,
     var toBePaid: Number = 0,
     var totalPrice: Number = 0,
-    var orderBillingModelList: List<OrderBillingModel>
-)
+    //var orderBillingModelList: List<OrderBillingModel>
+) : Parcelable
