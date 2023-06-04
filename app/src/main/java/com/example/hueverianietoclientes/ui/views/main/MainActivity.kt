211f3abe11e65b551dc32f4454d7da2a01902486
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-04 18:49:21 +0200

    feat: Add logout feature

--- app/src/main/java/com/example/hueverianietoclientes/ui/views/main/MainActivity.kt
+++ app/src/main/java/com/example/hueverianietoclientes/ui/views/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.hueverianietoclientes.ui.views.main
 
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
@@ -16,6 +18,9 @@ import com.example.hueverianietoclientes.base.BaseActivity
 import com.example.hueverianietoclientes.base.BaseState
 import com.example.hueverianietoclientes.data.network.ClientData
 import com.example.hueverianietoclientes.databinding.ActivityMainBinding
+import com.example.hueverianietoclientes.ui.components.HNModalDialog
+import com.example.hueverianietoclientes.utils.Utils
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,6 +32,9 @@ class MainActivity : BaseActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private val mainViewModel : MainViewModel by viewModels()
     lateinit var clientData: ClientData
+    private lateinit var alertDialog: HNModalDialog
+
+
     override fun setUp() {
         clientData = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.getParcelableExtra("client_data", ClientData::class.java)!!
@@ -45,6 +53,8 @@ class MainActivity : BaseActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         configNav(false)
+
+        this.alertDialog = HNModalDialog(this)
     }
 
     override fun configureUI() {
@@ -72,6 +82,36 @@ class MainActivity : BaseActivity() {
 
     fun goBackFragments() {
         onBackPressedDispatcher.onBackPressed()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                Utils.setPopUp(
+                    alertDialog,
+                    this,
+                    "Aviso",
+                    "¿Estás seguro de que quieres cerrar sesión?",
+                    "Cancelar",
+                    "Continuar",
+                    {
+                        alertDialog.cancel()
+                    },
+                    {
+                        alertDialog.cancel()
+                        FirebaseAuth.getInstance().signOut()
+                        this.mainViewModel.navigateToLogin(applicationContext, this)
+                    }
+                )
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
