84ee0ca0fe04576689b6a4b83d7cfbed2b24630a
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-15 22:55:57 +0200

    feat: Create changepasswordviewstate

---
+++ app/src/main/java/com/example/hueverianietoclientes/ui/views/main/fragment/changepassword/ChangePasswordViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianietoclientes.ui.views.main.fragment.changepassword
+
+import com.example.hueverianietoclientes.base.BaseState
+
+class ChangePasswordViewState (
+    var isLoading: Boolean = false,
+    var error: Boolean = false,
+    var isEmpty: Boolean =  false
+) : BaseState
