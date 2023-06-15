ac38440778fec24386a820da266afd5c84c366f0
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-15 22:56:40 +0200

    feat: Make changePasswordUseCase

--- app/src/main/java/com/example/hueverianietoclientes/domain/usecase/ChangePasswordUseCase.kt
+++ app/src/main/java/com/example/hueverianietoclientes/domain/usecase/ChangePasswordUseCase.kt
@@ -1,5 +1,15 @@
 package com.example.hueverianietoclientes.domain.usecase
 
+import com.example.hueverianietoclientes.data.network.ChangePasswordService
 import javax.inject.Inject
 
-class ChangePasswordUseCase @Inject constructor()
+class ChangePasswordUseCase @Inject constructor(
+    private val changePasswordService: ChangePasswordService
+) {
+
+    suspend operator fun invoke(oldPass: String, newPass: String) : Boolean {
+        val result = changePasswordService.changePassword(oldPass, newPass)
+        return result.isSuccess
+    }
+
+}
